35fd3fc346140ab9d86743b9b46288fea4feced8
Ridri98 <167820993+Ridri98@users.noreply.github.com>
2026-08-05 14:27:11 -0300

    Comment out node-fetch import in main.js
    Comment out the import statement for node-fetch.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const fetch = require('node-fetch');
+//const fetch = require('node-fetch');
 
 const PORT = process.env.PORT || 3000;
 app.use(cors());
